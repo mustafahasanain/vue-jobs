e6cfa1bc290647ce682a5812e604270be867a602
Mustafa Hasanain <m7asanain@gmail.com>
2025-09-28 12:46:05 +0300

    creating home cards and card container component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Test Title" subtitle="Test Subtitle" />
+  <Hero
+    title="Become a Vue Dev"
+    subtitle="Find the Vue job that fits your skills and needs"
+  />
+  <HomeCards />
 </template>
