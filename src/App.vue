60a8358da69925be3139e82a71e66def3bb0631a
Mustafa Hasanain <m7asanain@gmail.com>
2025-09-29 15:02:13 +0300

    job listing component and json data

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
+import JobListing from "./components/JobListing.vue";
 </script>
 
 <template>
@@ -11,4 +13,6 @@ import HomeCards from "./components/HomeCards.vue";
     subtitle="Find the Vue job that fits your skills and needs"
   />
   <HomeCards />
+  <JobListings />
+  <JobListing />
 </template>
