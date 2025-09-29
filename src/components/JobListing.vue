915e116c995df0f802a2de21e727c49d2cab9c6e
Mustafa Hasanain <m7asanain@gmail.com>
2025-09-29 20:28:25 +0300

    computed() and truncated description

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,8 +1,24 @@
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref, computed } from "vue";
 
-defineProps({
+const props = defineProps({
   job: Object,
+});
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
+
+const trancatedDescription = computed(() => {
+  let description = props.job.description;
+
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + "...";
+  }
+
+  return description;
 });
 </script>
 
@@ -15,7 +31,15 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <div>
+          {{ trancatedDescription }}
+        </div>
+        <button
+          @click="toggleFullDescription"
+          class="text-green-500 hover:text-green-600 mb-5"
+        >
+          {{ showFullDescription ? "Less" : "More" }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
