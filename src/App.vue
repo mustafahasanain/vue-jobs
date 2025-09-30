170dd0c8001d012aeb7b34a1b86e0878824ec075
Mustafa Hasanain <m7asanain@gmail.com>
2025-09-30 10:57:35 +0300

    using vue router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCards from "./components/HomeCards.vue";
-import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Navbar />
-  <Hero
-    title="Become a Vue Dev"
-    subtitle="Find the Vue job that fits your skills and needs"
-  />
-  <HomeCards />
-  <JobListings limit="3" :showButton="true" />
+  <RouterView />
 </template>
