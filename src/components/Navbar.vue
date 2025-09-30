682a3f5c16f01237355ffd34b11374a037570613
Mustafa Hasanain <m7asanain@gmail.com>
2025-09-30 11:24:19 +0300

    navbar active link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { RouterLink, useRoute } from "vue-router";
 import logo from "@/assets/img/logo.png";
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
 </script>
 
 <template>
@@ -19,20 +24,48 @@ import logo from "@/assets/img/logo.png";
           </RouterLink>
           <div class="md:ml-auto">
             <div class="flex space-x-2">
+              <!-- " rounded-md px-3 py-2" -->
               <RouterLink
                 to="/"
-                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/')
+                    ? 'bg-green-900'
+                    : 'hover:bg-gray-900 hover:text-white',
+                  ,
+                  'text-white',
+                  'rounded-md',
+                  'px-3',
+                  'py-2',
+                ]"
                 >Home</RouterLink
               >
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs')
+                    ? 'bg-green-900'
+                    : 'hover:bg-gray-900 hover:text-white',
+                  ,
+                  'text-white',
+                  'rounded-md',
+                  'px-3',
+                  'py-2',
+                ]"
                 >Jobs</RouterLink
               >
               <RouterLink
                 to="/jobs/add"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                >Add Job</RouterLink
+                :class="[
+                  isActiveLink('/jobs/add')
+                    ? 'bg-green-900'
+                    : 'hover:bg-gray-900 hover:text-white',
+                  ,
+                  'text-white',
+                  'rounded-md',
+                  'px-3',
+                  'py-2',
+                ]"
+                >Add Jobs</RouterLink
               >
             </div>
           </div>
