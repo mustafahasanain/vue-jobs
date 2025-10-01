db1653aaff9e69a2152359131655ccf167595f24
Mustafa Hasanain <m7asanain@gmail.com>
2025-10-01 15:06:41 +0300

    create add job page and form

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
@@ -21,6 +22,11 @@ const router = createRouter({
       path: "/jobs/:id",
       name: "job",
       component: JobView,
+    },
+    {
+      path: "/jobs/add",
+      name: "-addjob",
+      component: AddJobView,
     },
     {
       path: "/:catchAll(.*)",
